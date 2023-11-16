e5e69e3a3d915633aded2a93f699c35e24dc4073
Carlos Raúl Robinson Thompson <127860791+Spartan1404@users.noreply.github.com>
2023-11-16 18:50:19 -0500

    Update UDPServer.py

--- Programing assignments/UDP Pinger/UDPServer/UDPServer.py
+++ Programing assignments/UDP Pinger/UDPServer/UDPServer.py
@@ -1,13 +1,13 @@
-import random  # Import random library
-from socket import *  # Import socket library
+import random  
+from socket import * 
 
-serverSocket = socket(AF_INET, SOCK_DGRAM)  # Create a UDP socket for the server
-serverSocket.bind(('127.0.0.1', 12000))  # Set IP Address and Port Number of Socket
-print("Started UDP Server IP Address: 127.0.0.1 and Port: 12000")  # Print string on screen
-while True:  # Run program forever
-    rand = random.randint(0, 10)  # Probability 100%
+serverSocket = socket(AF_INET, SOCK_DGRAM)  # Crea socket udp para el servidor
+serverSocket.bind(('10.0.0.2', 4000))  # Direcion ip y puerto
+print("Server Running...") 
+while True: 
+    rand = random.randint(0, 10)  # simulando perdida de paquetes
     message, address = serverSocket.recvfrom(1024)
-    message = message.upper()  # Convert client message to uppercase letter
-    if rand < 6:  # < 60% without reply message
+    message = message.upper()  
+    if rand < 6:  # < 60% de perdida
         continue
-    serverSocket.sendto(message, address)  # Send the uppercase message back to the client
+    serverSocket.sendto(message, address)  # Mandar el mensaje de vuelta al cliente
