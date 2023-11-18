9d9f7a062fbca04cf65112b7f44c6bdb06bc2ade
InfoMarq <151186410+InfoMarq@users.noreply.github.com>
2023-11-17 21:35:06 -0500

    Update MultiThreadServer.py

--- Programing assignments/Web Server/Opcional web server multihilo/MultiThreadServer.py
+++ Programing assignments/Web Server/Opcional web server multihilo/MultiThreadServer.py
@@ -1,5 +1,4 @@
 from socket import *
-import datetime
 import threading
 
 class ClientThread(threading.Thread):
@@ -7,66 +6,57 @@ class ClientThread(threading.Thread):
 		threading.Thread.__init__(self)
 		self.connectionSocket = connect
 		self.addr = address
+		
+		print('nueva conexion', address)
 	def run(self):
+	
 		while True:
 			try:
-				message = connectionSocket.recv(1024)
+				message = self.connectionSocket.recv(1024)
 				if not message:
 					break
-				print "message: \n", message
-				filename = message.split()[1]
-				f = open(filename[1:])
-				outputdata = f.read() 
-				print "outputdata:", outputdata
-				now = datetime.datetime.now() 
-				#Send one HTTP header line into socket
-				first_header = "HTTP/1.1 200 OK"
-				# alive ={
-				# 	"timeout":10,
-				# 	"max":100,
-				# }
-				header_info = {
-					# "Date": now.strftime("%Y-%m-%d %H:%M"),
-					"Content-Length": len(outputdata),
-					"Keep-Alive": "timeout=%d,max=%d" %(10,100),
-					"Connection": "Keep-Alive",
-					"Content-Type": "text/html"
-				}
+				print("\nmessage: ", message)
+				data = str(message.decode())
+				print("\ndecode: ", data)
+				filename = data.split()[1]
+				print("\nfilename:", filename[0:])
+				f = open(filename[0:])
+				outputdata = f.read()
 				
-				following_header = "\r\n".join("%s:%s" % (item, header_info[item]) for item in header_info)
-				print "following_header:", following_header
-				connectionSocket.send("%s\r\n%s\r\n\r\n" %(first_header, following_header))
-
-				# connectionSocket.send("\r\n")
-				 # Date: %s\r\nKeep-Alive: timeout=10, max=100\r\n Connection: nKeep-Alive\r\n Content-Type: text/html;charset= utf-8" %(now.strftime("%Y-%m-%d %H:%m")))
-				#Send the content of the requested file to the client
+				connectionSocket.send(bytes('HTTP/1.1 200 OK\r\n\r\n','UTF-8'))
 				for i in range(0, len(outputdata)):
-					connectionSocket.send(outputdata[i])
+					connectionSocket.send(bytes(outputdata[i],'UTF-8'))
+				connectionSocket.send(bytes("\r\n",'UTF-8'))
+				connectionSocket.shutdown(SHUT_WR)
+				#self.connectionSocket.close()
+				
 			except IOError:
-				#Send response message for file not found
-				connectionSocket.send("HTTP/1.1 404 Not Found\r\n\r\n")
-				# Content-Type:text/html\r\n\r\n<!doctype html><html><body><h1>404 Not Found<h1></body></html>
-				#Close client socket 
+				self.connectionSocket.send(bytes('HTTP/1.1 404 Not Found\r\n\r\n','UTF-8'))
+				self.connectionSocket.send(bytes('<html><head><title>First Web Page</title></head><body><h1>404 Not Found</h1></body></html>\r\n','UTF-8'))
+				connectionSocket.shutdown(SHUT_WR)
+				#self.connectionSocket.close()
+				
 
 if __name__ == '__main__':
 	serverSocket = socket(AF_INET, SOCK_STREAM) #Prepare a sever socket
-	serverPort = 6789
-	serverSocket.bind(('',serverPort))
+	#Fill in start
+	serverPort = 5000
+	serverSocket.bind(('10.0.0.2',serverPort))
 	serverSocket.listen(5)
 	threads=[]
+	#Fill in end
 	while True:
 		#Establish the connection
-		print 'Ready to serve...'
-		connectionSocket, addr = serverSocket.accept()
-		print "addr:\n", addr
-		client_thread = ClientThread(connectionSocket,addr)
-		client_thread.start()
-		client_thread.setDaemon(True)
-		threads.append(client_thread)
+		try:
+			print('Ready to serve...')
+			connectionSocket, addr = serverSocket.accept()
+			client_thread = ClientThread(connectionSocket,addr)
+			client_thread.daemon = True
+			client_thread.start()
+			threads.append(client_thread)
+		except error:
+			print("socket error")
+			break
+
 	#main thread wait all threads finish then close the connection
-	"""
-	# for thread in threads:
-	# 	thread.join()
-	If I put this, Chrome will not gonna work, safari will work.
-	"""
 	serverSocket.close()
